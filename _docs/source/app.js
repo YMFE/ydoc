8e3a34f804117eb1368fa1f44c0f55b9a460c4bc
wanglili <wang508x102@163.com>
2016-09-05 17:55:09 +0800

    update

--- _docs/source/app.js
+++ _docs/source/app.js
@@ -15,10 +15,26 @@ $(document).ready(function() {
     }
     });
 
-  $('.docs-sidenav li').click(function(e){
-      if($(this).next('ul')){
-          $(this).next('ul').toggle();
+  $('.docs-sidenav>li').click(function(e){
+      if($(this).hasClass('active')){
+          $(this).removeClass('active');
+      }else{
+        $(this).addClass('active').siblings('li').removeClass('active');
       }
-      return false;
+      if($(this).next('ul')){
+          $(this).next('ul').toggle().siblings('ul').hide();
+      }
+     return false;
   });
+  $(window).scroll(function(e){
+      $('.docs-sidenav').width($('.ydoc-container-content').width()*0.25);
+      $('.docs-sidenav').css({
+          'left': $(window).width()/2-$('.ydoc-container-content').width()/2
+      });
+      if($(this).scrollTop() >=  $('.ydoc-container-content').scrollTop()){
+          $('.docs-sidenav').addClass('fixed');
+      }else{
+          $('.docs-sidenav').removeClass('fixed');
+      }
+  })
 })
