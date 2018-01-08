7b5f57e6ff872550f7d29318522a94809ecec2fc
wenbo.dong <wenbo.dong@qunar.com>
2018-01-08 17:11:10 +0800

    feat: 修改样式 整理目录结构

--- theme/template/Head.jsx
+++ theme/template/Head.jsx
@@ -1,6 +1,6 @@
 <head>
 	<title>{props.data.title}</title>
-	<link rel="stylesheet" href={relePath(props.data.page.distPath, "documents/style/style.css")} />
+	<link rel="stylesheet" href={relePath(props.data.page.distPath, "plugins/style.css")} />
 
 
 
