4c90dea35b28de4fc6e253c92a724bc9e419bcc5
wenbo.dong <wenbo.dong@qunar.com>
2018-01-10 22:54:13 +0800

    feat: 抽屉导航适配PC

--- theme/template/Summary.jsx
+++ theme/template/Summary.jsx
@@ -1,26 +1,37 @@
 {
 	// console.log(JSON.stringify(props.content, null, 2))
+	function testz() {
+		console.log('rerere');
+		
+	}
 }
 
 <div className="m-summary">
-{
-	(() => {
-		const getItems = (articles) => {
-			return articles.map((item) => {
-				return <li className="item">{item.title}</li>;
-			});
-		};
-		
-		return props.content.map((item) => {
-			console.log(item);
+	{
+		(() => {
+			const testz = () => {
+				console.log('-=-=-=-==--==--==--=');
+				
+			}
+			const getItems = (articles) => {
+				return articles.map((item) => {
+					return <li className="item">{item.title}</li>;
+				});
+			};
 			
-			return (
-				<div className="m-summary-block">
-					{item.title ? <div className="m-summary-title">{item.title}</div> : ''}
-					<ul className="m-summary-list">{getItems(item.articles)}</ul>
-				</div>
-			);
-		});
-	})()
-}
+			return props.content.map((item) => {
+				console.log(item);
+				
+				return (
+					<div className="m-summary-block">
+						{item.title ? <div className="m-summary-title">{item.title}</div> : ''}
+						<ul className="m-summary-list">{getItems(item.articles)}</ul>
+					</div>
+				);
+			});
+		})()
+	}
+	<div className="m-summary-switch js-summaty-switch" onclick={() => console.log('eeee')
+	
+	}>&#xf020;</div>
 </div>
