0faf47fec45c3147793643cc0b3b8ded8dc69e00
suxiaoxin <hellosean1025@163.com>
2018-01-11 14:36:07 +0800

    fix: highlignh

--- src/parse/markdown.js
+++ src/parse/markdown.js
@@ -1,10 +1,19 @@
 const marked = require('marked')
 const fs = require('fs');
 const utils = require('../utils');
+const hljs = require('highlight.js');
+
+marked.setOptions({
+  highlight: function (code, lang) {
+    if(lang && hljs.getLanguage(lang)){
+      return hljs.highlight(lang, code).value;
+    }else return hljs.highlightAuto(code).value;
+  }
+});
 
 function parser(filepath) {
   let contents = fs.readFileSync(filepath, 'utf8');
-  var renderer = new marked.Renderer();
+  const renderer = new marked.Renderer();
   renderer.heading = function (text, level) {
     text = utils.hashEncode(text);
     return `<h${level} id="${text}">${text}</h${level}>`
