99c921b78e8fee6237334b8ef3f2e8970b81c84a
wenbo.dong <wenbo.dong@qunar.com>
2018-01-17 17:37:46 +0800

    feat: homepage header

--- theme/template/Content.jsx
+++ theme/template/Content.jsx
@@ -1,12 +1,11 @@
 
 {
-  // // console.log(JSON.stringify(props.content, null, 2))
-  
+  // console.log(JSON.stringify(props, null, 2))
 }
 
 <div className="m-content">
   <div className="m-content-container markdown-body">
-    <h1 className="title">Heading H1</h1>
+    {/* <h1 className="title">Heading H1</h1> */}
     <div dangerouslySetInnerHTML={{ __html: props.content }}></div>
   </div>
 </div>
