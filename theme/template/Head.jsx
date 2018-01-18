ae078f40ad1a1cdfe4ea2e6384444def5b6430bb
wenbo.dong <wenbo.dong@qunar.com>
2018-01-18 19:20:08 +0800

    feat: 调整favicon模板所在位置

--- theme/template/Head.jsx
+++ theme/template/Head.jsx
@@ -1,11 +1,5 @@
 <head>
 	<title>{props.title}</title>
-	<link rel="apple-touch-icon" sizes="180x180" href={relePath(props.distPath, 'ydoc/images/apple-touch-icon.png')} />
-	<link rel="icon" type="image/png" sizes="32x32" href={relePath(props.distPath, 'ydoc/images/favicon-32x32.png')} />
-	<link rel="icon" type="image/png" sizes="16x16" href={relePath(props.distPath, 'ydoc/images/favicon-16x16.png')} />
-	<link rel="manifest" href={relePath(props.distPath, 'ydoc/images/manifest.json')} />
-	<link rel="mask-icon" href={relePath(props.distPath, 'ydoc/images/safari-pinned-tab.svg"')} color="#5bbad5" />
-	<meta name="theme-color" content="#ffffff" />
 	<link rel="stylesheet" href={relePath(props.distPath, 'ydoc/styles/style.css')} />
 	{props.asserts.css.map(item=>{
 		return <link key={item} rel="stylesheet" href={relePath(props.distPath, item)} />
