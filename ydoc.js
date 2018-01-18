7b8ee7fb2dc8b3105791c1cefc15a0b63805b7b6
suxiaoxin <hellosean1025@163.com>
2018-01-18 17:28:47 +0800

    opti: docs

--- ydoc.js
+++ ydoc.js
@@ -1,12 +1,12 @@
 module.exports = {
   title: "ydoc demo",
   description: "ydoc description demo",
-  author: "ymfe",
-  plugins: [{
-    name: 'import-assert',
-    options: {
-      css: 'custom.css',
-      js:  'custom.js'
-    }
-  }]
+  author: "ymfe"
+  // plugins: [{
+  //   name: 'import-assert',
+  //   options: {
+  //     css: 'custom.css',
+  //     js:  'custom.js'
+  //   }
+  // }]
 }
