8ce7352ea5b91236365089488fd94fce2ca37aeb
wenbo.dong <wenbo.dong@qunar.com>
2018-01-22 23:43:09 +0800

    docs: 修改旧版注释

--- commetparser/Button.js
+++ commetparser/Button.js
@@ -33,6 +33,22 @@ const defaultProps = {
 
 const propTypes = {
     /**
+     * 滚动组件
+     *
+     * @component ScrollView
+     * @examplelanguage js
+     * @version >=0.20.0
+     * @foldnumber 8 代码块折叠行数
+     * @description 一个包装了平台的ScrollView（滚动视图）的组件，同时还集成了触摸锁定的“响应者”系统。
+     *
+     * * 记住ScrollView必须有一个确定的高度才能正常工作，因为它实际上所做的就是将一系列不确定高度的子组件装进一个确定高度的容器（通过滚动操作）。
+     * * 要给一个ScrollView确定一个高度的话，要么直接给它设置高度（不建议），要么确定所有的父容器都已经绑定了高度。
+     * * 在视图栈的任意一个位置忘记使用{flex:1}都会导致错误，你可以使用元素查看器来查找问题的原因。
+     *
+     *
+     */
+
+    /**
      * 头部描述随意
      * 可以换行写描述
      * 
