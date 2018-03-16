06e0ccd2573d80bb431a2efd1918d7bbbb5fdeba
wenbo.dong <wenbo.dong@qunar.com>
2018-03-16 14:27:16 +0800

    opti: 快速翻页结构调整

--- theme/components/Content.jsx
+++ theme/components/Content.jsx
@@ -9,13 +9,17 @@
           <div className="m-content-container m-paging">
             {props.prev ?
               <div className="m-paging-prev m-paging-item">
-                <span className="ui-font-ydoc">&#xf07d;</span>
-                <a href={props.prev.releativePath} class="href">{props.prev.title}</a>
+                <a href={props.prev.releativePath} className="href">
+                  <span className="ui-font-ydoc">&#xf07d;</span>
+                  {props.prev.title}
+                </a>
               </div> : null}
             {props.next ?
               <div className="m-paging-next m-paging-item">
-                <a href={props.next.releativePath} class="href">{props.next.title}</a>
-                <span className="ui-font-ydoc">&#xf07f;</span>
+                <a href={props.next.releativePath} className="href">
+                  {props.next.title}
+                  <span className="ui-font-ydoc">&#xf07f;</span>
+                </a>
               </div> : null}
           </div>
         ) : null
