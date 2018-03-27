50b68e155845225954722669e11c2592e9d15a35
wenbo.dong <wenbo.dong@qunar.com>
2018-03-27 16:53:41 +0800

    opti: 按钮动画效果

--- docs/plugin/index.jsx
+++ docs/plugin/index.jsx
@@ -1,7 +1,7 @@
 ---
 plugins: 
   - { title: 'ydoc-plugin-copy', href: 'https://www.npmjs.com/package/ydoc-plugin-copy', desc: '用于快速复制 markdown 生成页面的代码片段。' }
-  - { title: '插件一', href: '#', desc: '插件一插件一插件一插件一插件一插件一插件一' }
+  - { title: 'ydoc-plugin-import-asset', href: 'https://www.npmjs.com/package/ydoc-plugin-import-asset', desc: '在页面中引入 js 与 css 文件' }
   - { title: '插件一', href: '#', desc: '插件一插件一插件一插件一插件一插件一插件一' }
   - { title: '插件一', href: '#', desc: '插件一插件一插件一插件一插件一插件一插件一' }
   - { title: '插件一', href: '#', desc: '插件一插件一插件一插件一插件一插件一插件一' }
