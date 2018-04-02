e735da4702619c11e849a88566d5f24d5082caf5
wenbo.dong <wenbo.dong@qunar.com>
2018-04-02 12:28:12 +0800

    docs: 修改版本信息

--- docs/index.jsx
+++ docs/index.jsx
@@ -5,7 +5,7 @@ banner:
   btns: 
     - { name: '开 始', href: './documents/index.html', primary: true }
     - { name: 'Github >', href: 'https://github.com/YMFE/ydoc' }
-  caption: '当前版本: v4.0.1'
+  caption: '当前版本: v4.0.2'
 features: 
   - { name: '优雅', desc: '经过精雕细琢，我们带给大家一个精心设计的、拥有卓越的视觉与交互体验的文档构建工具' }
   - { name: '灵动', desc: '我们拥有非常灵活的 插件机制 与 主题定制 功能，正在努力构建活跃的插件社区。也许初次使用未见其惊艳，但当你灵活使用插件后便会发现她的强大' }
