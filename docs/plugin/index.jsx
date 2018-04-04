87c68ca9f3d5cf442c108779fe47eccbb5fd87f6
wenbo.dong <wenbo.dong@qunar.com>
2018-04-04 16:17:04 +0800

    docs: 插件列表更新

--- docs/plugin/index.jsx
+++ docs/plugin/index.jsx
@@ -1,9 +1,11 @@
 ---
 plugins: 
-  - { title: 'ydoc-plugin-copy', href: 'https://www.npmjs.com/package/ydoc-plugin-copy', desc: '用于快速复制 markdown 生成页面的代码片段。' }
   - { title: 'ydoc-plugin-import-asset', href: 'https://www.npmjs.com/package/ydoc-plugin-import-asset', desc: '在页面中引入 js 与 css 文件' }
+  - { title: 'ydoc-plugin-search', href: 'https://www.npmjs.com/package/ydoc-plugin-search', desc: '为 YDoc 提供搜索功能' }
+  - { title: 'ydoc-plugin-copy', href: 'https://www.npmjs.com/package/ydoc-plugin-copy', desc: '用于快速复制 markdown 生成页面的代码片段。' }
   - { title: 'ydoc-plugin-jsdoc', href: 'https://www.npmjs.com/package/ydoc-plugin-jsdoc', desc: '根据代码注释生成文档，基于 jsdoc' }
   - { title: 'ydoc-plugin-pangu', href: 'https://www.npmjs.com/package/ydoc-plugin-pangu', desc: '自动替你在网页中所有的中文字和半形的英文、数字、符号之间插入空白(盘古之白)' }
+  - { title: 'ydoc-plugin-img-view', href: 'https://www.npmjs.com/package/ydoc-plugin-pangu', desc: '自动替你在网页中所有的中文字和半形的英文、数字、符号之间插入空白(盘古之白)' }
 ---
 <div className="m-content-container m-pluginbox">
 {
