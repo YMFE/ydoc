eed1d53a41f0cc9d781bfde55383d4a1f7dd644c
wenbo.dong <wenbo.dong@qunar.com>
2018-10-10 18:01:56 +0800

    chore: Version to 4.1.0

---
+++ _site/examples/init/search_json.js
@@ -0,0 +1,45 @@
+window.ydoc_plugin_search_json = {
+  "documents": [
+    {
+      "title": "ydoc",
+      "content": "This is home page of documents book.",
+      "url": "/documents/index.html",
+      "children": []
+    },
+    {
+      "title": "Installation",
+      "content": "...",
+      "url": "/documents/installation.html",
+      "children": []
+    },
+    {
+      "title": "Introduction",
+      "content": "Introduction",
+      "url": "/documents/intro.html",
+      "children": [
+        {
+          "title": "title",
+          "url": "/documents/intro.html#title",
+          "content": "titlecontent"
+        }
+      ]
+    },
+    {
+      "title": "env",
+      "content": "env",
+      "url": "/documents/env.html",
+      "children": [
+        {
+          "title": "title",
+          "url": "/documents/env.html#title",
+          "content": "titlecontent"
+        },
+        {
+          "title": "sub-title",
+          "url": "/documents/env.html#title-sub-title",
+          "content": "sub-titlecontent"
+        }
+      ]
+    }
+  ]
+}
