32b7a859f65bbb03ee3005aa1ea9ed49bfd2ba01
苏文雄 <suwenxiong@meituan.com>
2019-09-20 10:30:09 +0800

    opti: 优化 build

--- bin/build-demo-page.sh
+++ bin/build-demo-page.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# 构建 ydoc 的页面
-bin/ydoc build
-
 # 构建子页面
 cd examples/init && ../../bin/ydoc build && cp -r ./_site ../../_site/examples/init/
 cd ../homepage && ../../bin/ydoc build && cp -r ./_site ../../_site/examples/homepage/
